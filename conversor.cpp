e08f4c547b50daecfce9fb75bcb457ce580c450e
MaikiMerino11 <maikijunior9@gmail.com>
2023-01-05 23:04:18 -0500

    primera estructura del proyecto

---
+++ conversor.cpp
@@ -0,0 +1,77 @@
+#include "conversor.h"
+#include "ui_conversor.h"
+
+Conversor::Conversor(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::Conversor)
+{
+    ui->setupUi(this);
+    // Conectar el dial de Centigrados con el slot de ESTA clase
+    connect(ui->inCent, SIGNAL(valueChanged(int)),
+            this, SLOT(cent2fahr(int)));
+    connect(ui->inFahr, SIGNAL(valueChanged(int)),
+            this, SLOT (fahr2cent(int)));
+    connect(ui->inKel, SIGNAL(valueChanged(int)),
+            this, SLOT (kel2cent(int)));
+    connect(ui->inCent, SIGNAL(valueChanged(int)),
+            this, SLOT(cent2kel(int)));
+    connect(ui->inFahr, SIGNAL(valueChanged(int)),
+            this, SLOT (fahr2kel(int)));
+    connect(ui->inKel, SIGNAL(valueChanged(int)),
+            this, SLOT (kel2fahr(int)));
+}
+
+Conversor::~Conversor()
+{
+    delete ui;
+}
+
+void Conversor::cent2fahr(int grados)
+{
+    if (ui->inCent->hasFocus()){
+        int f = (grados * 9.0/5) + 32;
+        ui->inFahr->setValue(f);
+    }
+
+}
+
+void Conversor::fahr2cent(int grados)
+{
+    if (ui->inFahr->hasFocus()){
+        int c = (grados - 32) * 5.0/9.0;
+        ui->inCent->setValue(c);
+    }
+}
+
+void Conversor::kel2cent(int grados)
+{
+    if (ui->inKel->hasFocus()){
+        int c = (grados - 273.15);
+        ui->inCent->setValue(c);
+    }
+}
+
+void Conversor::cent2kel(int grados)
+{
+    if (ui->inCent->hasFocus()){
+        int k = (grados + 273.15);
+        ui->inKel->setValue(k);
+    }
+}
+
+void Conversor::fahr2kel(int grados)
+{
+    if (ui->inFahr->hasFocus()){
+        int k = ((grados - 32) * 5.0/9.0) + 273.15;
+        ui->inKel->setValue(k);
+    }
+}
+
+void Conversor::kel2fahr(int grados)
+{
+    if (ui->inKel->hasFocus()){
+        int f = (1.8 * (grados - 273)) + 32;
+        ui->inFahr->setValue(f);
+    }
+}
+
---
+++ conversor.h
@@ -0,0 +1,29 @@
+#ifndef CONVERSOR_H
+#define CONVERSOR_H
+
+#include <QDialog>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Conversor; }
+QT_END_NAMESPACE
+
+class Conversor : public QDialog
+{
+    Q_OBJECT
+
+public:
+    Conversor(QWidget *parent = nullptr);
+    ~Conversor();
+
+public slots:
+    void cent2fahr(int grados);
+    void cent2kel(int grados);
+    void fahr2cent(int grados);
+    void fahr2kel(int grados);
+    void kel2cent(int grados);
+    void kel2fahr(int grados);
+
+private:
+    Ui::Conversor *ui;
+};
+#endif // CONVERSOR_H
